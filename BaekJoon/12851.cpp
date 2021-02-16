28a83e04c3cf56ed3adbd42422063b07ee7bddf9
husk <68003176+JangHanjun@users.noreply.github.com>
2021-02-16 13:26:58 +0900

    2021.02.16

--- BaekJoon/12851.cpp
+++ BaekJoon/12851.cpp
@@ -4,46 +4,47 @@ using namespace std;
 #define max 100001
 bool isVisited[max];  // 방문한 곳을 다시 방문할 필요가 없으니 체크하기 위한 배열 (default : false)
 int N, K;
-int result= 0;
+int result = 0;
+
 int search(int N, int K) {
     int count = 0;      // 몇번만에 만났는지 체크할 변수
-    int minCount = 0;
     queue<int> q;
     q.push(N);      // 일단 자기 위치 넣어줌
-
+    isVisited[N] = true;
+    int minCount = 0;
+    
     while(!q.empty()){    //계속 돌게하고 탈출 조건인 N = K를 안에 넣음
         int n = q.size();
         for(int i = 0; i < n; i++){
 
             N = q.front();
             q.pop();
-            
-            if(N == K && minCount == count){
+            isVisited[N] = true;
+
+            if(count == minCount && N == K){
                 result++;
             }
-            if(!minCount && N == K){  // 첫 완료
+            if(!minCount && N == K){
                 minCount = count;
                 result++;
             }
+            
             if (N > 0 && isVisited[N-1] == false){
                 q.push(N-1);
-                isVisited[N-1] = true;
             }
             if (N < 100001 && isVisited[N+1] == false){
                 q.push(N+1);
-                isVisited[N+1] = true;
             }
             if (2*N < 100001 && isVisited[N*2] == false){
                 q.push(N*2);
-                isVisited[N*2] = true;
             }
         }
         count++;    //이번 레벨에서 없었으니 다음 레벨로
     }
     return minCount;
 }
-int main(){
 
+int main(){
     scanf("%d %d", &N, &K);
     printf("%d\n", search(N,K));
     printf("%d\n", result);
