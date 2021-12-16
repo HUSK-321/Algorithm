3a7e7b8c759671aacc8ec3e203bf73be5fb05853
husk <68003176+JangHanjun@users.noreply.github.com>
2021-12-16 20:07:04 +0900

    21.12.16

---
+++ BaekJoon/2668.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int n, answer = 0;
+int visit[101];
+int nums[101];
+vector<int> ans;
+
+
+void HaveCycle(int start, int cur){
+    if(visit[cur]){
+        if(start == cur)
+            ans.push_back(start);
+    }
+    else {
+        visit[cur] = true;
+        HaveCycle(start, nums[cur]);
+    }
+}
+
+int main(){
+
+    // inputs
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        cin >> nums[i];
+    
+    // DFS
+    for(int i = 1; i <= n; i++){
+        HaveCycle(i, i);
+        // TODO : 이거 bool 안되는듯?
+        memset(visit, 0, sizeof(visit));
+    }
+
+    // Answer
+    answer = ans.size();
+
+    cout << answer << '\n';
+    for(int i = 0; i < answer; i++){
+        cout << ans[i] << '\n';
+    }
+
+}
