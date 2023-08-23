5cf0212c2722a7a975845add85fa9120f125cd98
Husk <68003176+HUSK-321@users.noreply.github.com>
2023-08-23 18:22:15 +0900

    branchless 실험

--- example.cpp
+++ example.cpp
@@ -1,25 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <chrono>
 #include <string>
-
 using namespace std;
 
-int main(){
-    string s;
-    cin >> s;
-    int num = 0;
-    int x = 0;
+string GenerateRandomString() {
+    static const char alphabet[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
 
-    for(int i = s.size() - 1; i >= 0; i--){
-        if(s[i] == '1'){
-           num += (1 << x); 
+    string resultString;
+    resultString.reserve(1000000000);
+
+    for (int i = 0; i < 1000000000; ++i) {
+        resultString += alphabet[rand() % (sizeof(alphabet) - 1)];
+    }
+    
+    return resultString;
+}
+
+void BranchConverter(string& s)
+{
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] -= 32;
         }
-        x++;
+    }
+}
+
+void BranchlessConverter(string& s)
+{
+    for(int i = 0; i < s.size(); i++)
+    {
+        s[i] -= 32 * (s[i] >= 'a' && s[i] <= 'z');
+    }
+}
+
+int main()
+{
+    {
+        cout << "1. Convert with Branch\n";
+        string testString = GenerateRandomString();
+        auto start = chrono::system_clock::now();
+
+        BranchConverter(testString);
+
+        auto end = chrono::system_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << duration.count() << "\n ============================\n";
     }
 
-    cout << num << '\n';
+    {
+        cout << "2. Convert with Branchless\n";
+        string testString = GenerateRandomString();
+        auto start = chrono::system_clock::now();
 
-    cin >> hex >> num;
-    cout << num << oct << num << '\n';
+        BranchlessConverter(testString);
+
+        auto end = chrono::system_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << duration.count() << "\n ============================\n";
+    }
 }
